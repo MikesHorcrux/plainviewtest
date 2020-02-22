c37810dacaecc4f7de6650ec88874643d86807ed
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-22 14:44:55 -0600

    gutted axios, was not working the way I needed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-import {grabPVData} from './services/planviewAPI'
 
 export const App = () => {
-        const [data, setData] = useState([]);
-
-        useEffect(()=>{
-            grabPVData().then(APIData => setData(APIData));
-        },[])
-
+       
     return(
-        <h2> {JSON.stringify(data) } </h2>
+        <h2> hey Mike</h2>
     );
 }
