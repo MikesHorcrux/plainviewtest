6391b15c9362d84425c176a573db38262b37e5bb
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-22 09:13:50 -0600

    - get function to grab api data

---
+++ src/services/planviewAPI.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//declaring api url 
+//you may also use this for more complex urls and have a base url here instead.
+export const url = 'https://planview-media.s3.us-west-2.amazonaws.com/interview/sessions.json';
+
+
+//grabbing data from api with axios get 
+export const grabPVData = async => {
+    const result = await axios.get(url).then(data => data);
+    return result;
+}
