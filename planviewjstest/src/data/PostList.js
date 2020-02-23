7441e9d63d9eb88bd289bc6bc70f0fc13183576f
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-23 15:38:16 -0600

    //see track in console need to group by track

--- planviewjstest/src/data/PostList.js
+++ planviewjstest/src/data/PostList.js
@@ -1,20 +1,16 @@
 import React, { Component} from "react";
 import PlanView from "../data/pvdata.json";
 
-
 class PostList extends Component {
     
     render(){
         
         return(
             <div>
-                {/* {PlanView.map((item)=> {
-                    return(
-                    <p> {item.Track}</p>
-                    )
-                })} */}
-                {JSON.stringify(PlanView.Items[1].Track)}
-                {console.log(PlanView)}
+                
+                {
+                    //I can grab the track in the console
+                console.log(PlanView.Items[0].Track)}
             </div>
         )
     }
