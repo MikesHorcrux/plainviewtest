4c205ecb59e01408da71bf06c48910ac775936fd
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-23 18:16:26 -0600

    -type error in groupBy func

--- planviewjstest/src/data/PostList.js
+++ planviewjstest/src/data/PostList.js
@@ -1,16 +1,31 @@
 import React, { Component} from "react";
 import PlanView from "../data/pvdata.json";
 
+//gouping all the arrays in an array by thier track
+const groupBy = (key) => {
+    return PlanView.reduce((result, currentValue) => {
+      // If an array already present for key, push it to the array. 
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      return result;
+    }, {}); // empty object is the initial value for result object
+  };
+  const groupedByTrack = groupBy('Track');
+  
+  
+
 class PostList extends Component {
-    
     render(){
         
         return(
             <div>
                 
                 {
-                    //I can grab the track in the console
-                console.log(PlanView.Items[0].Track)}
+                console.log(groupedByTrack)
+                
+                
+                }
             </div>
         )
     }
