d2df06895552d3abf7270dd3f5ee98fcdeef322a
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-25 10:16:19 -0600

    -- func test added

--- planviewjstest/src/data/PostList.js
+++ planviewjstest/src/data/PostList.js
@@ -1,39 +1,29 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import lodash from "lodash";
 import PlanView from "../data/pvdata.json";
-
-
-
-  
-  
-
 class PostList extends Component {
-    render(){
-      //Groups Data By Track 
-      const groupedByTrack = lodash.groupBy(PlanView.Items, 'Track.Title')
-      //console.log(groupedByTrack)
-      //console.log(PlanView)
-       
-      
-      function test(track){
-        
-        //  for (var i = 0; i < groupedByTrack.track.length; i++) {
-        //  console.log(groupedByTrack.track[i].Title);
-        //  console.log(groupedByTrack.track[i].Description);
-        //  console.log("...")
-        console.log(track)
-            console.log(groupedByTrack.track)
-            console.log("..........")
-            console.log(groupedByTrack.Build)
-         }
-         test("Build")
-
-         
-    
-        return(
-        <h1>test</h1>
-        )
+  render() {
+    //Groups Data By Track 
+    const groupedByTrack = lodash.groupBy(PlanView.Items, 'Track.Title')
+    console.log(PlanView.Items, groupedByTrack)
+    //console.log(PlanView)
+    function test(track) {
+      for (var key in groupedByTrack) {
+        groupedByTrack[key].map(track => {
+          console.log(track.Title);
+          console.log(track.Description)
+          console.log("...")
+        });
+        // console.log(track)
+        // console.log(groupedByTrack.track)
+        // console.log("..........")
+        // console.log(groupedByTrack.Build)
+      }
     }
+    test("Interact")
+    return (
+      <h1>test</h1>
+    )
+  }
 }
-
 export default PostList
