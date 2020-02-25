bf35fdf5f67358f083b4a9745ab614763ba81d95
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-02-25 10:51:24 -0600

    --replaced lodash with const groupBY

--- planviewjstest/src/data/PostList.js
+++ planviewjstest/src/data/PostList.js
@@ -4,9 +4,29 @@ import PlanView from "../data/pvdata.json";
 class PostList extends Component {
   render() {
     //Groups Data By Track 
-    const groupedByTrack = lodash.groupBy(PlanView.Items, 'Track.Title')
+    // const groupedByTrack = lodash.groupBy(PlanView.Items, 'Track.Title')
+
+    const groupBy = (array ,key) => {
+        return array.reduce((result, currentValue) => {
+          // If an array already present for key, push it to the array. 
+          (result[currentValue[key].Title] = result[currentValue[key]] || []).push(
+            currentValue
+          );
+          return result;
+        }, {}); // empty object is the initial value for result object
+      };
+      const groupedByTrack = groupBy(PlanView.Items, 'Track');
+
+
+
+
     console.log(PlanView.Items, groupedByTrack)
     //console.log(PlanView)
+
+
+
+
+
     function test(track) {
       for (var key in groupedByTrack) {
         groupedByTrack[key].map(track => {
